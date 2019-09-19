bdca7c13807ce72f3c4f7b61282ff0853e387176
TBinh <thanh.binh@primelabo.com.vn>
2019-09-19 15:30:51 +0700

    Fix Layout

--- application/views/booking/index.php
+++ application/views/booking/index.php
@@ -1,45 +1,34 @@
+    <div class="row-fluid">
+        <div class="span12">
 
+            <h2>Booking &nbsp;</h2>
 
-    <style type="text/css">
-        .block a:hover{
-            color: silver;
-        }
-        .block a{
-            color: #fff;
-        }
-        .block {
-            position: fixed;
-            background: #2184cd;
-            padding: 20px;
-            z-index: 1;
-            top: 240px;
-        }
-    </style>
-  
-    <div>
-        <div style="margin-top:20px">
+            <div class="row-fluid">
+                <div class="span12">Calendar of my Booking Room.</div>
+            </div>
 
-
-
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/book/js/script.js"></script>
-
-<link href="<?php echo base_url(); ?>assets/book/css/fullcalendar.css" rel="stylesheet" />
-<link href="<?php echo base_url(); ?>assets/book/css/fullcalendar.print.css" rel="stylesheet" media="print" />
-<script src="<?php echo base_url(); ?>assets/book/js/moment.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/book/js/fullcalendar.js"></script>
-
-
-<!-- add calander in this div -->
-<div class="container">
-  <div class="row">
-<div id="calendar"></div>
-
-</div>
-</div>
+            <!-- <div class="row-fluid">
+                <div class="span6">
+                    <button id="cmdPrevious" class="btn btn-primary"><i class="mdi mdi-chevron-left"></i></button>
+                    <button id="cmdToday" class="btn btn-primary">today</button>
+                    <button id="cmdNext" class="btn btn-primary"><i class="mdi mdi-chevron-right"></i></button>
+                </div>
+                <div class="span6">
+                    <div class="pull-right">
+                        <button id="cmdDisplayDayOff" class="btn btn-primary"><i class="mdi mdi-calendar"></i>&nbsp;Days off</button>
+                    </div>
+                </div>
+            </div> -->
+            <div class="row-fluid">
+                <div class="span12">&nbsp;</div>
+            </div>
+            <div id='calendar'></div>
+        </div>
+    </div>
 
 
 <!-- Modal -->
-<div id="createEventModal" class="modal fade" role="dialog">
+<div id="createEventModal" class="modal hide fade">
   <div class="modal-dialog">
 
     <!-- Modal content-->
@@ -55,18 +44,18 @@
                     <input class="form-control" id="title" name="title" placeholder="Event" type="text" value="">
                 </div>
             </div>
-            
+
             <input type="hidden" id="startTime"/>
             <input type="hidden" id="endTime"/>
-            
-            
-       
+
+
+
         <div class="control-group">
             <label class="control-label" for="when">When:</label>
             <div class="controls controls-row" id="when" style="margin-top:5px;">
             </div>
         </div>
-        
+
       </div>
       <div class="modal-footer">
         <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
@@ -78,7 +67,7 @@
 </div>
 
 
-<div id="calendarModal" class="modal fade">
+<div id="calendarModal" class="modal hide fade">
 <div class="modal-dialog">
     <div class="modal-content">
         <div class="modal-header">
@@ -99,6 +88,13 @@
 </div>
 <!--Modal-->
 
+<link href="<?php echo base_url();?>assets/fullcalendar-2.8.0/fullcalendar.css" rel="stylesheet">
+<script type="text/javascript" src="<?php echo base_url();?>assets/fullcalendar-2.8.0/lib/moment.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url();?>assets/fullcalendar-2.8.0/fullcalendar.min.js"></script>
+<?php if ($language_code != 'en') {?>
+<script type="text/javascript" src="<?php echo base_url();?>assets/fullcalendar-2.8.0/lang/<?php echo strtolower($language_code);?>.js"></script>
+<?php }?>
+<script src="<?php echo base_url();?>assets/js/bootbox.min.js"></script>
+<script src="<?php echo base_url();?>assets/js/clipboard-1.6.1.min.js"></script>
 
-<div style='margin-left: auto;margin-right: auto;text-align: center;'>
-</div>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/book/js/script.js"></script>
