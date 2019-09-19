55df85e8ffe68c93f115dfed4d279cf98bcf80e4
TBinh <thanh.binh@primelabo.com.vn>
2019-09-19 16:43:06 +0700

    Layout

--- application/views/booking/index.php
+++ application/views/booking/index.php
@@ -7,18 +7,18 @@
                 <div class="span12">Calendar of my Booking Room.</div>
             </div>
 
-            <!-- <div class="row-fluid">
+            <div class="row-fluid">
                 <div class="span6">
                     <button id="cmdPrevious" class="btn btn-primary"><i class="mdi mdi-chevron-left"></i></button>
                     <button id="cmdToday" class="btn btn-primary">today</button>
                     <button id="cmdNext" class="btn btn-primary"><i class="mdi mdi-chevron-right"></i></button>
                 </div>
-                <div class="span6">
+                <!-- <div class="span6">
                     <div class="pull-right">
                         <button id="cmdDisplayDayOff" class="btn btn-primary"><i class="mdi mdi-calendar"></i>&nbsp;Days off</button>
                     </div>
-                </div>
-            </div> -->
+                </div> -->
+            </div>
             <div class="row-fluid">
                 <div class="span12">&nbsp;</div>
             </div>
@@ -88,13 +88,135 @@
 </div>
 <!--Modal-->
 
-<link href="<?php echo base_url();?>assets/fullcalendar-2.8.0/fullcalendar.css" rel="stylesheet">
-<script type="text/javascript" src="<?php echo base_url();?>assets/fullcalendar-2.8.0/lib/moment.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url();?>assets/fullcalendar-2.8.0/fullcalendar.min.js"></script>
+<div class="modal hide" id="frmModalAjaxWait" data-backdrop="static" data-keyboard="false">
+        <div class="modal-header">
+            <h1><?php echo lang('global_msg_wait'); ?></h1>
+        </div>
+        <div class="modal-body">
+            <img src="<?php echo base_url(); ?>assets/images/loading.gif"  align="middle">
+        </div>
+ </div>
+
+<link href="<?php echo base_url(); ?>assets/fullcalendar-2.8.0/fullcalendar.css" rel="stylesheet">
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/fullcalendar-2.8.0/lib/moment.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/fullcalendar-2.8.0/fullcalendar.min.js"></script>
 <?php if ($language_code != 'en') {?>
-<script type="text/javascript" src="<?php echo base_url();?>assets/fullcalendar-2.8.0/lang/<?php echo strtolower($language_code);?>.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/fullcalendar-2.8.0/lang/<?php echo strtolower($language_code); ?>.js"></script>
 <?php }?>
-<script src="<?php echo base_url();?>assets/js/bootbox.min.js"></script>
-<script src="<?php echo base_url();?>assets/js/clipboard-1.6.1.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/bootbox.min.js"></script>
+<script src="<?php echo base_url(); ?>assets/js/clipboard-1.6.1.min.js"></script>
+
+
+
+<script type="text/javascript">
+
+$(function () {
+    //Global Ajax error handling mainly used for session expiration
+    $( document ).ajaxError(function(event, jqXHR, settings, errorThrown) {
+        $('#frmModalAjaxWait').modal('hide');
+        if (jqXHR.status == 401) {
+            bootbox.alert("<?php echo lang('global_ajax_timeout'); ?>", function() {
+                //After the login page, we'll be redirected to the current page
+               location.reload();
+            });
+        } else { //Oups
+            bootbox.alert("<?php echo lang('global_ajax_error'); ?>");
+        }
+    });
+
+    var calendar = $('#calendar').fullCalendar({
+        timeFormat: ' ', /*Trick to remove the start time of the event*/
+        header: {
+            left: "",
+            center: "title",
+            right: ""
+        },
+        defaultView: 'agendaWeek',
+        editable: true,
+        selectable: true,
+        allDaySlot: false,
+
+        events: "/booking/loadData",
+
+
+        eventClick: function(event, jsEvent, view) {
+            endtime = $.fullCalendar.moment(event.end).format('h:mm');
+            starttime = $.fullCalendar.moment(event.start).format('dddd, MMMM Do YYYY, h:mm');
+            var mywhen = starttime + ' - ' + endtime;
+            $('#modalTitle').html(event.title);
+            $('#modalWhen').text(mywhen);
+            $('#eventID').val(event.id);
+            $('#calendarModal').modal();
+        },
+
+        //header and other values
+        select: function(start, end, jsEvent) {
+            endtime = $.fullCalendar.moment(end).format('h:mm');
+            starttime = $.fullCalendar.moment(start).format('dddd, MMMM Do YYYY, h:mm');
+            var mywhen = starttime + ' - ' + endtime;
+            start = moment(start).format();
+            end = moment(end).format();
+            $('#createEventModal #startTime').val(start);
+            $('#createEventModal #endTime').val(end);
+            $('#createEventModal #when').text(mywhen);
+            $('#createEventModal').modal('toggle');
+        },
+        eventDrop: function(event, delta) {
+            console.log(event);
+            $.ajax({
+                url: "booking/update",
+                data: {
+                    'title': event.title,
+                    'start': moment(event.start).format(),
+                    'end': moment(event.end).format(),
+                    'id': event.id
+                },
+                type: "POST",
+                success: function(json) {
+                    //alert(json);
+                }
+            });
+        },
+        eventResize: function(event) {
+            console.log(event);
+            $.ajax({
+                url: "booking/update",
+                data: {
+                    'title': event.title,
+                    'start': moment(event.start).format(),
+                    'end': moment(event.end).format(),
+                    'id': event.id
+                },
+                type: "POST",
+                success: function(json) {
+                    //alert(json);
+                }
+            });
+        }
+    });
+
+    //Manage Prev/Next buttons
+    $('#cmdNext').click(function() {
+        $('#calendar').fullCalendar('next');
+    });
+    $('#cmdPrevious').click(function() {
+        $('#calendar').fullCalendar('prev');
+    });
+
+    //On click on today, if the current month is the same than the displayed month, we refetch the events
+    $('#cmdToday').click(function() {
+        // var displayedDate = new Date($('#calendar').fullCalendar('getDate'));
+        // var currentDate = new Date();
+        // if (displayedDate.getMonth() == currentDate.getMonth()) {
+        //     $('#calendar').fullCalendar('today');
+        // } else {
+            $('#calendar').fullCalendar('today');
+        // }
+    });
+
+    
+});
+
+</script>
 
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/book/js/script.js"></script>
