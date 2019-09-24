96049aafa5df74557a912d12051c28a35314d4c1
TBinh <thanh.binh@primelabo.com.vn>
2019-09-24 17:04:30 +0700

    Fixed Layout

--- application/views/booking/index.php
+++ application/views/booking/index.php
@@ -11,12 +11,16 @@
     <div class="span12">
         <h2>Booking</h2>
         <div class="row-fluid">
-            <div class="span12"><?php echo lang('calendar_individual_description'); ?></div>
+            <div class="span12">
+                <?php echo lang('calendar_individual_description'); ?>
+            </div>
         </div>
         <div class="row-fluid">
             <div class="span6">
                 <button id="cmdPrevious" class="btn btn-primary"><i class="mdi mdi-chevron-left"></i></button>
-                <button id="cmdToday" class="btn btn-primary"><?php echo lang('today'); ?></button>
+                <button id="cmdToday" class="btn btn-primary">
+                    <?php echo lang('today'); ?>
+                </button>
                 <button id="cmdNext" class="btn btn-primary"><i class="mdi mdi-chevron-right"></i></button>
             </div>
             <div class="span6">
@@ -25,68 +29,68 @@
                 </div>
             </div>
         </div>
-        <div class="row-fluid"><div class="span12">&nbsp;</div></div>
+        <div class="row-fluid">
+            <div class="span12">&nbsp;</div>
+        </div>
         <div id='calendar'></div>
     </div>
 </div>
 <!-- Modal -->
 <div id="createEventModal" class="modal hide fade">
-  <div class="modal-dialog">
+    <div class="modal-dialog">
 
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Add Event</h4>
-      </div>
-      <div class="modal-body">
-            <div class="control-group">
-                <label class="control-label" for="inputPatient">Event:</label>
-                <div class="field desc">
-                    <input class="form-control" id="title" name="title" placeholder="Event" type="text" value="">
-                </div>
+        <!-- Modal content-->
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Add Event</h4>
             </div>
+            <div class="modal-body">
+                <div class="control-group">
+                    <label class="control-label" for="inputPatient">Event:</label>
+                    <div class="field desc">
+                        <input class="form-control" id="title" name="title" placeholder="Event" type="text" value="">
+                    </div>
+                </div>
 
-            <input type="hidden" id="startTime"/>
-            <input type="hidden" id="endTime"/>
-			<input type="hidden" id="roomid"/>
+                <input type="hidden" id="startTime" />
+                <input type="hidden" id="endTime" />
+                <input type="hidden" id="roomid" />
 
+                <div class="control-group">
+                    <label class="control-label" for="when">When:</label>
+                    <div class="controls controls-row" id="when" style="margin-top:5px;">
+                    </div>
+                </div>
 
-        <div class="control-group">
-            <label class="control-label" for="when">When:</label>
-            <div class="controls controls-row" id="when" style="margin-top:5px;">
+            </div>
+            <div class="modal-footer">
+                <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
+                <button type="submit" class="btn btn-primary" id="submitButton">Save</button>
             </div>
         </div>
 
-      </div>
-      <div class="modal-footer">
-        <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
-        <button type="submit" class="btn btn-primary" id="submitButton">Save</button>
     </div>
-    </div>
-
-  </div>
 </div>
-
 
 <div id="calendarModal" class="modal hide fade">
-<div class="modal-dialog">
-    <div class="modal-content">
-        <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title">Event Details</h4>
-        </div>
-        <div id="modalBody" class="modal-body">
-        <h4 id="modalTitle" class="modal-title"></h4>
-        <div id="modalWhen" style="margin-top:5px;"></div>
-        </div>
-        <input type="hidden" id="eventID"/>
-        <div class="modal-footer">
-            <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
-            <button type="submit" class="btn btn-danger" id="deleteButton">Delete</button>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Event Details</h4>
+            </div>
+            <div id="modalBody" class="modal-body">
+                <h4 id="modalTitle" class="modal-title"></h4>
+                <div id="modalWhen" style="margin-top:5px;"></div>
+            </div>
+            <input type="hidden" id="eventID" />
+            <div class="modal-footer">
+                <button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
+                <button type="submit" class="btn btn-danger" id="deleteButton">Delete</button>
+            </div>
         </div>
     </div>
-</div>
 </div>
 <!--Modal-->
 <link href="<?php echo base_url(); ?>assets/fullcalendar-2.8.0/fullcalendar.css" rel="stylesheet">
